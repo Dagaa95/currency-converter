a92b502267dc25b840be7d32476800b4a6fb0cbf
Dagmara Piatkowska <dagaa1@hotmail.com>
2023-07-10 12:52:57 +0200

    const added

--- js/script.js
+++ js/script.js
@@ -1,33 +1,49 @@
-let formElement = document.querySelector(".js-form")
-let amountElement = document.querySelector(".js-amount")
-let currencyElement = document.querySelector(".js-currency")
-let resultElement = document.querySelector(".js-result")
+{
+  const calculateResult = (amount, currency) => {
+    const rateEUR = 4.4786;
+    const rateUSD = 4.1150;
+    const rateGBP = 5.2296;
 
-let rateEUR = 4.4786;
-let rateUSD = 4.1150;
-let rateGBP = 5.2296;
+    switch (currency) {
+      case "EUR":
+        return amount / rateEUR;
 
-formElement.addEventListener("submit", (event) => {
-  event.preventDefault();
+      case "USD":
+        return amount / rateUSD;
 
-  let amount = +amountElement.value;
-  let currency = currencyElement.value;
+      case "GBP":
+        return amount / rateGBP;
+    }
+  };
 
-  let result;
 
-  switch (currency) {
-    case "EUR":
-      result = amount / rateEUR;
-      break;
-
-    case "USD":
-      result = amount / rateUSD;
-      break;
-
-    case "GBP":
-      result = amount / rateGBP;
-      break;
+  const updateResultText = (amount, result, currency) => {
+    const resultElement = document.querySelector(".js-result");
+    resultElement.innerHTML = `${amount.toFixed(2)} PLN = ${result.toFixed(2)} ${currency}`;
   }
 
-  resultElement.innerHTML = `${amount.toFixed(2)} PLN = ${result.toFixed(2)} ${currency}`;
-})
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const amountElement = document.querySelector(".js-amount");
+    const currencyElement = document.querySelector(".js-currency");
+
+    const amount = +amountElement.value;
+    const currency = currencyElement.value;
+
+    const result = calculateResult(amount, currency);
+
+    updateResultText(amount, result, currency);
+  };
+
+  const init = () => {
+    const formElement = document.querySelector(".js-form");
+
+    formElement.addEventListener("submit", onFormSubmit);
+  };
+
+  init();
+};
+
+
